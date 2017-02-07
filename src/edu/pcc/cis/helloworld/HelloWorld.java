06e3a04d87bcdd59bc6c186a38b43aa91a26caad
Steven Farring <steven.farring@pcc.edu>
2017-02-07 12:53:38 -0800

    added main function that prints hello world message

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -4,4 +4,7 @@ package edu.pcc.cis.helloworld;
  * Created by sfarring on 2/7/2017.
  */
 public class HelloWorld {
+        public static void main (String[] args) {
+            System.out.println("Hello World");
+        }
 }
