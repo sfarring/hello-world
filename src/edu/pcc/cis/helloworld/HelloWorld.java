dfe3da5367ad190c8a70422605c775ff51953fab
Steven Farring <steven.farring@pcc.edu>
2017-02-07 14:12:26 -0800

    added name

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -3,10 +3,11 @@ import java.util.Date;
 
 /**
  * Created by sfarring on 2/7/2017.
+ * @author Steven Farring
  */
 public class HelloWorld {
         public static void main (String[] args) {
-            System.out.println("Hello World");
+            System.out.println("Hello World from Steven Farring");
             System.out.println(new Date());
         }
 }
